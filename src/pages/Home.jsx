0e1d3f8c166a15f722dbaf49520fdb76568ae6fa
KEEN <kyh0927@live.co.kr>
2019-09-25 00:21:14 +0900

    190924

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { CSSTransition } from 'react-transition-group';
-import '../style/transition.scss';
-
+import { CSSTransition} from 'react-transition-group';
+import '../../src/style/transition.scss';
 const Home = () => {
     return (
         <CSSTransition
-            transitionName="homeTransition"
-            transitionAppear={true}
-            transitionAppearTimeout={500}
-            transitionEnter={false}
-            transitionLeave={false}>
-            <div>
-                Home
-            </div>
+            in={true}
+            timeout={100}
+            classNames='switch-transition'
+            // unmountOnExit
+            appear
+            // enter
+        >
+          <div>HOME</div>
         </CSSTransition>
+            
+            
+        
     )
 }
 
